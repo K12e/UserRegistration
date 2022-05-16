a7c545d477a3ca6d3118b7758c6f901852b5edb0
k12e <kalyanirathod2095@gmail.com>
2022-05-16 22:27:54 +0530

    Added : Welcome messgae added

---
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To User Registration");
+        System.out.println("Enter 1 : validate first name");
+
+    }
+}
---
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validateFirstName("Kalyani");
+        Assertions.assertTrue(result);
+    }
+}
+
